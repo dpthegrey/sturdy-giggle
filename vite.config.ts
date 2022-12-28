5a85b3f521f231fc6cb1f7e743a95352318a38c9
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-12-28 19:23:12 +0530

    added base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/sturdy-giggle/",
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
